71c1f3781ce25d889f6eb70d63edd43a7308796f
hud-k <hudkh4n@gmail.com>
2026-07-27 21:06:14 +0100

    feat: added routes for each template

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,20 @@ import sqlalchemy
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
+    @app.route("/home")
     def homepage():
         return render_template("home.html")
     
-    @app.route("/about")
-    def about():
-        return "practicing routes"
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
     
-    @app.route("/secret")
-    def secret():
-        return "you shouldnt be here!"
+    @app.route("/log-workout")
+    def log_workout():
+        return render_template("log_workout.html")
+
+    @app.route("/history")
+    def history():
+        return render_template("history.html")
 
     return app
