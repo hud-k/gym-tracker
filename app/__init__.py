b22154adc0871c2af8683b3a9e52df4bae2582c9
hud-k <hudkh4n@gmail.com>
2026-07-30 20:28:19 +0100

    feat: added post handling + register page

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
     def homepage():
         return render_template("home.html")
+
+    @app.route("/register")
+    def register():
+        return render_template("register.html")
     
-    @app.route("/login")
+    @app.route("/login", methods=["GET", "POST"])
     def login():
+        if request.method == "POST":
+            return "You submitted information"
+            
         return render_template("login.html")
     
     @app.route("/log-workout")
