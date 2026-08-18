f041bbf5952f0f3acdea81b0c443a8788568434a
hud-k <hudkh4n@gmail.com>
2026-08-18 19:30:10 +0100

    feat: add workout model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,20 @@ class Users(db.Model, UserMixin):
         def __init__(self, username, password):
             self.username = username
             self.password = password
+
+class Workout(db.Model):
+      id = db.Column(db.Integer, primary_key=True)
+      user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+      exercise_name = db.Column(db.String(100))
+      weight = db.Column(db.Float)
+      sets = db.Column(db.Integer)
+      reps = db.Column(db.Integer)
+      date = db.Column(db.Date)
+
+      def __init__(self, user_id, exercise_name, weight, sets, reps, date):
+            self.user_id = user_id
+            self.exercise_name = exercise_name
+            self.weight = weight
+            self.sets = sets
+            self.reps = reps
+            self.date = date
